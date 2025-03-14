2678aa726d91b7e6f8119c825d906744ae039ca2
luisff73 <jvrluis@hotmail.com>
2025-03-14 17:41:40 +0100

    vuelta atras

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-  },
+  }
 });
